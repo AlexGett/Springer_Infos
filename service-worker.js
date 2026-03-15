ecf296a20f13f045e8dfccfa694dbfc5dfbd5f88
AlexGett <alex_gettinger@yahoo.com>
2026-03-15 18:31:01 +0100

    Update cache version to v1.1.8

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'behälter-rechner-v1.1.7'; // Cache Version aktualisiert
+const CACHE_NAME = 'behälter-rechner-v1.1.8'; // Cache Version aktualisiert
 const urlsToCache = [
 	'./',
 	'./index.html',
